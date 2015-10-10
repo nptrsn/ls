9e12257919dd327cfecd653ef81d02164dba1fbe
nptrsn <nick@hindiemedia.com>
2015-10-10 14:44:07 -0700

    background styling

--- footer.php
+++ footer.php
@@ -1,7 +1,4 @@
 <div id="footer">
-  <ul id="bottomnav">
-    <?php wp_nav_menu(array('theme_location' => 'footer_nav')); ?>
-  </ul>
   <p>&copy; <?php echo date('Y') ?> <a href="<?php bloginfo('url') ?>">
     <?php bloginfo('name') ?>
     </a>
--- index.php
+++ index.php
@@ -1,32 +1,11 @@
 <?php get_header(); ?>
 
-  <div id="main">
-    <div id="content">
-    
-    	<?php if (!is_single() && !is_page() && !is_front_page()) : ?><h1><?php wp_title(' ', true, 'right'); ?></h1><?php endif; ?>
-        
-        <?php /* begin the loop */ if (have_posts()) : ?>
-		
-		<?php while (have_posts()) : the_post(); ?>
-        
-			<?php if (is_page()) : /* show page contents */ ?>
-
-          <div class="main">
-            <video autoplay loop muted poster="vid-first-frame.jpg" class="home-bg">
-              <source src="media/ls.mov" type="video/mov">
-            </video>
-          </div>
-       
-            <?php endif; /* end if page or post */ ?>
-        
-        <?php endwhile;/* end the main loop */ ?>
-        
-        <?php endif; /* end if have_posts */ ?>      
-    
-    
+  <div class="main-wrap">
+    <div class="content">
+        <video autoplay loop muted poster="media/ls.png" class="home-bg" src="http://lucidsolutions.io/wp-content/themes/ls/media/ls.mov" type="video/mov">
+        </video>
+      </div>
     </div><!-- / end content -->
-    
-    
-  </div><!-- / end main -->
+  </div><!-- / end main wrap-->
 
 <?php /* footer */ get_footer(); ?>
