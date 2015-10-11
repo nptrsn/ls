5c0b9747f95897330e507ca9b2d66c7ef4400678
nptrsn <nick@hindiemedia.com>
2015-10-11 09:55:21 -0700

    services scroll up

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
         </video>
       </div>
     </div><!-- / end content -->
+
+    <div class="scrollUp">
+      <div class="services-wrap">
+        <h2>Services</h2>
+        <h3>CMS</h3>
+        <h3>Coding</h3>
+        <h3>Online Marketing</h3>
+      </div>
+    </div>
   </div><!-- / end main wrap-->
 
 <?php /* footer */ get_footer(); ?>
