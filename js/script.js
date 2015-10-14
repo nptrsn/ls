58ccc4f7b25076d39c7d44a28b03cb428c5a9806
nptrsn <nick@hindiemedia.com>
2015-10-13 23:16:11 -0700

    after responsive QA

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 jQuery(document).ready(function($){
   // Services Link
+  var mobile = 650;
+  var screenSize = $( window ).width();
+
   $('.link-services').click(function(){
     if( $('.contactWrap').hasClass('active')) {
-      $('contactWrap').removeClass('active');
+      $('.contactWrap').removeClass('active');
       $('.servicesWrap').addClass('active');
       $('.contactWrap').css({top: "2000px"});
-      $('.servicesWrap').animate({top: "160px"}, 750);
+      if ( screenSize > mobile) {
+        $('.servicesWrap').animate({top: "160px"}, 750);
+      }
+      else if ( screenSize < mobile) {
+        $('.servicesWrap').animate({top: "60px"}, 750);
+      }
     }
     $('.servicesWrap').addClass('active');
     $('.bodyCopy').slideUp('slow');
-    $('.servicesWrap').animate({top: "160px"}, 750);
+    if ( screenSize > mobile) {
+        $('.servicesWrap').animate({top: "160px"}, 750);
+      }
+      else if ( screenSize < mobile) {
+        $('.servicesWrap').animate({top: "60px"}, 750);
+      }
   });
   $('.home').hover(function(){
     $('.fa-arrow-right').effect('shake', { direction: 'right', times: 10, distance: 5}, 5000);
@@ -19,19 +32,35 @@ jQuery(document).ready(function($){
     $('.servicesWrap').removeClass('active');
     $('.contactWrap').addClass('active');
     $('.servicesWrap').css({top: "2000px"});
-    $('.contactWrap').animate({top: "-160px"}, 750);
+    if ( screenSize > mobile) {
+      $('.contactWrap').animate({top: "-160px"}, 750);
+    }
+    else if ( screenSize < mobile) {
+      $('.contactWrap').animate({top: "-260px"}, 750);
+    }
   });
 
   //Contact Link
   $('.link-contact').click(function(){
     if( $('.servicesWrap').hasClass('active')) {
-      $('servicesWrap').removeClass('active');
+      $('.servicesWrap').removeClass('active');
       $('.contactWrap').addClass('active');
       $('.servicesWrap').css({top: "2000px"});
-      $('.contactWrap').animate({top: "-160px"}, 750);
+      if ( screenSize > mobile) {
+        $('.contactWrap').animate({top: "-160px"}, 750);
+      }
+      else if ( screenSize < mobile) {
+        $('.contactWrap').animate({top: "-260px"}, 750);
+      }
     }
     $('.contactWrap').addClass('active');
+    $('servicesWrap').removeClass('active');
     $('.bodyCopy').slideUp('slow');
-    $('.contactWrap').animate({top: "-160px"}, 750);
+    if ( screenSize > mobile) {
+      $('.contactWrap').animate({top: "-160px"}, 750);
+      }
+      else if ( screenSize < mobile) {
+        $('.contactWrap').animate({top: "-260px"}, 750);
+      }
   });
 });
